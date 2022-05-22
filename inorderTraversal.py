0776562bb80e448b053fd98bf7059e7db41e62ca
Matthew Anzano <63332109+MattAnzano@users.noreply.github.com>
2022-05-22 15:30:21 -0400

    Create inorderTraversal.py
    Inorder traversal of binary Tree

---
+++ inorderTraversal.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    treeNodes = []
+    
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        treeNodes = []
+        if root != None:
+            self.inorderTraversal(root.left)
+            self.treeNodes.append(root.val)
+            self.inorderTraversal(root.right)
+            
+        return self.treeNodes
